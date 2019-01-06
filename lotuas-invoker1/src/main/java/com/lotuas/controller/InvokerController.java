017e602869630047d17fda0b31aa965bfc517b91
biejunyang <biejunyang1210@163.com>
2019-01-06 17:26:17 +0800

    eureka健康自检实现

--- lotuas-invoker1/src/main/java/com/lotuas/controller/InvokerController.java
+++ lotuas-invoker1/src/main/java/com/lotuas/controller/InvokerController.java
@@ -1,14 +1,20 @@
 package com.lotuas.controller;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EurekaDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @Configuration
@@ -21,13 +27,34 @@ public class InvokerController {
     }
 
 
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+
     @GetMapping("/hello")
     public String hello(String name){
-        Map<String, Object> paramMap=new HashMap<>();
-        paramMap.put("name", "zhangsan");
-        String result=restTemplate().getForObject("http://lotuas-sp1/hello", String.class, paramMap);
+        String result=restTemplate().getForObject("http://lotuas-sp1/hello?name="+name, String.class);
         return result;
     }
 
+    @GetMapping("/router")
+    public void router(){
+        List<ServiceInstance> ins=getServiceInstances();
+        for(ServiceInstance service: ins){
+            EurekaDiscoveryClient.EurekaServiceInstance esi=(EurekaDiscoveryClient.EurekaServiceInstance)service;
+            InstanceInfo info=esi.getInstanceInfo();
+            System.out.println(info.getAppName()+"----"+info.getInstanceId()+"---"+info.getStatus());
+        }
+    }
+
+    private List<ServiceInstance> getServiceInstances(){
+        List<String> serviceIds=discoveryClient.getServices();
+        List<ServiceInstance> result=new ArrayList<>();
+        for(String sid: serviceIds){
+            result.addAll(discoveryClient.getInstances(sid));
+        }
+
+        return result;
+    }
 
 }
--- lotuas-sp1/src/main/java/com/lotuas/sp/controller/HelloController.java
+++ lotuas-sp1/src/main/java/com/lotuas/sp/controller/HelloController.java
@@ -1,14 +1,25 @@
 package com.lotuas.sp.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
+    public static boolean canVistDb=false;
+
+
     @GetMapping("/hello")
     public String hello(String name){
         System.out.println("hello, "+name);
         return "hello, " + name;
+    }
+
+
+    @GetMapping("/db/{canVistDb}")
+    public String setConnectState(@PathVariable("canVistDb") Boolean canVistDb){
+        HelloController.canVistDb=canVistDb;
+        return "当前数据库连接是否正常："+canVistDb;
     }
 }

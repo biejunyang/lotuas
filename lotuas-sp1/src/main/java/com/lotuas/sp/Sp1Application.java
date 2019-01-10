ea782c8c44d3f490ce4e5ad488048bf7488de10b
biejunyang <biejunyang1210@163.com>
2019-01-10 22:49:22 +0800

    ribbon study

--- lotuas-sp1/src/main/java/com/lotuas/sp/Sp1Application.java
+++ lotuas-sp1/src/main/java/com/lotuas/sp/Sp1Application.java
@@ -2,14 +2,21 @@ package com.lotuas.sp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;;import java.util.Scanner;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class Sp1Application {
 
     public static void main(String[] args){
-        SpringApplication.run(Sp1Application.class, args);
+//        SpringApplication.run(Sp1Application.class, args);
+
+        Scanner scanner=new Scanner(System.in);
+        String port=scanner.nextLine();
+        new SpringApplicationBuilder(Sp1Application.class)
+                .properties("server.port="+port).run(args);
+
     }
 }
 
--- lotuas-sp1/src/main/java/com/lotuas/sp/controller/HelloController.java
+++ lotuas-sp1/src/main/java/com/lotuas/sp/controller/HelloController.java
@@ -1,8 +1,11 @@
 package com.lotuas.sp.controller;
 
+import com.lotuas.sp.dto.Person;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RestController
 public class HelloController {
@@ -21,5 +24,16 @@ public class HelloController {
     public String setConnectState(@PathVariable("canVistDb") Boolean canVistDb){
         HelloController.canVistDb=canVistDb;
         return "当前数据库连接是否正常："+canVistDb;
+    }
+
+
+    @GetMapping("/findPerson/{personId}")
+    public Person findPerson(@PathVariable("personId") Integer personId, HttpServletRequest request){
+        Person person=new Person();
+        person.setId(personId);
+        person.setName("张三");
+        person.setAge(30);
+        person.setMessage(request.getRequestURL().toString());
+        return person;
     }
 }
